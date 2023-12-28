664804b0bca84a7bbe7cafa411f7b159d6959fd5
Gary Springer <g.springer@draftkings.com>
2023-12-28 13:03:20 -0500

    add theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Button className="m-4" onClick={() => console.log("sweet")}>
-      Click me
-    </Button>
+    <ThemeProvider defaultTheme="dark">
+      <Button className="m-4" onClick={() => console.log("sweet")}>
+        Click me
+      </Button>
+    </ThemeProvider>
   </React.StrictMode>
 );
